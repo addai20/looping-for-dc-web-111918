51c0230ea18a0ebab6bd3ac6afc22da7ed1380da
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-14 16:52:10 +0000

    automatically backed up by learn

--- for.rb
+++ for.rb
@@ -1,9 +1,18 @@
 
 def using_for
-	checklist = 1..10
+  checklist = 1..10
+  
 	for checklist do
 	  puts "Wingardium Leviosa"
 	end
 end
 
+
+#EXAMPLE
+
+# shoe_count = 1..32
+ 
+# for shoe in shoe_count 
+#     puts "I'm putting a pair of shoes away."
+# end
 
